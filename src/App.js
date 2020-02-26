1c60587bb06075da2120ab423df376c8e7d9ba6f
candaceyw <candaceyw@gmail.com>
2020-02-26 14:55:08 -0500

    counter two

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className='App'>
-      <CounterOne />
+      {/* <CounterOne /> */}
+      <CounterTwo />
     </div>
   );
 }
