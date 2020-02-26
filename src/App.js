bed8b2539c37f79761a13c16b519947a48e6a746
candaceyw <candaceyw@gmail.com>
2020-02-26 14:36:57 -0500

    counter one

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from './components/CounterOne';
+
+function App() {
+  return (
+    <div className='App'>
+      <CounterOne />
+    </div>
+  );
+}
+
+export default App;
